28d24fbae2ef7fc94ef62964db733595f903c533
H Akash <hakash@Hs-MacBook-Air.local>
2025-12-16 11:00:34 +0530

    don

---
+++ js/App.js
@@ -0,0 +1,14 @@
+import Navbar from "../react/navbar";
+import Hero from "../react/Hero";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      {/* <h1>Canteen Connect</h1> */}
+    </div>
+  );
+}
+
+export default App;
