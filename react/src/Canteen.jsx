5f051b4b75736ca6b8ac8802e76ab14dc782edac
Bakahatake <aanush748@gmail.com>
2025-12-20 12:26:51 +0530

    god mode

--- react/src/Canteen.jsx
+++ react/src/Canteen.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import Navbar from "./navbar"; 
-import "./Canteen.css";
+import "./canteen.css";
 
 function Canteen() {
   const [items, setItems] = useState([]);
