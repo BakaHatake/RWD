b04ee0e7e70a2f68ae67539077dd37036cd05966
Bakahatake <aanush748@gmail.com>
2025-12-23 14:51:02 +0530

    removed urls

--- backend/server.js
+++ backend/server.js
@@ -9,22 +9,22 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-const allowedurls=[
-    "http://localhost:8080",
-    "https://rwd-eight.vercel.app",
-    "https://rwd-tau.vercel.app",
-    "http://127.0.0.1:5173",
-]
-app.use(cors({
-    origin:function(origin,callback){
-        if(!origin)return callback(null,true);
-        if(allowedurls.includes(origin)){
-            callback(null,true);
-        }else{
-            callback(new Error("Not allowed by cors"))
-        }
-    }
-}))
+// const allowedurls=[
+//     "http://localhost:8080",
+//     "https://rwd-eight.vercel.app",
+//     "https://rwd-tau.vercel.app",
+//     "http://127.0.0.1:5173",
+// ]
+// app.use(cors({
+//     origin:function(origin,callback){
+//         if(!origin)return callback(null,true);
+//         if(allowedurls.includes(origin)){
+//             callback(null,true);
+//         }else{
+//             callback(new Error("Not allowed by cors"))
+//         }
+//     }
+// }))
 app.get("/", (req, res) => {
     res.send("Backend working");
 });
