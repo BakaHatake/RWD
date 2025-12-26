6816404d5b54e7aa5a91324b48fa2b5f129e0051
Akashhogetapal <akash85486@gmail.com>
2025-12-27 00:44:00 +0530

    topup

--- react/src/jsxfolder/Menu.jsx
+++ react/src/jsxfolder/Menu.jsx
@@ -1,6 +1,7 @@
 import "../css/menu.css";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+const [profileOpen, setProfileOpen] = useState(false);
 
 function Menu() {
     const navigate = useNavigate();
@@ -133,9 +134,13 @@ function Menu() {
                             <circle cx="9" cy="20" r="1" />
                             <circle cx="20" cy="20" r="1" />
                         </svg>
+                        <Profile
+                       open={profileOpen}
+                         onClose={() => setProfileOpen(false)}
+                         />
                     </div>
 
-                    <div className="profile-icon" onClick={() => navigate("/profile")}>
+                    <div className="profile-icon" onClick={() => setProfileOpen(prev => !prev)}>
                         <svg width="24" height="24" viewBox="0 0 16 16" fill="white">
                             <path d="M8 7C9.65685 7 11 5.65685 11 4C11 2.34315 9.65685 1 8 1C6.34315 1 5 2.34315 5 4C5 5.65685 6.34315 7 8 7Z" />
                             <path d="M14 12C14 10.3431 12.6569 9 11 9H5C3.34315 9 2 10.3431 2 12V15H14V12Z" />
