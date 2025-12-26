b72d694a4411f947d5924496ec389314cfdd3ffd
Akashhogetapal <akash85486@gmail.com>
2025-12-27 01:28:19 +0530

    topup

--- react/src/jsxfolder/Canteen.jsx
+++ react/src/jsxfolder/Canteen.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
 import "../css/canteen.css";
+import Profile from "./profile";
 
 function Canteen() {
+   const [open, setOpen] = useState(false);
   const navigate = useNavigate();
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -137,21 +139,24 @@ function Canteen() {
             </g>
           </svg>
         </div>
-        <div class="title"><h1>Canteen Connect</h1></div>
-        <div class="right">
-          <div class="cart" onClick={() => navigate("/cart")}>
+        <div className="title"><h1>Canteen Connect</h1></div>
+        <div className="right">
+          <div className="cart" onClick={() => navigate("/cart")}>
             <svg viewBox="0 0 24 24" fill="none" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
               <path d="M7.2998 5H22L20 12H8.37675M21 16H9L7 3H4M4 8H2M5 11H2M6 14H2" />
               <circle cx="9" cy="20" r="1" />
               <circle cx="20" cy="20" r="1" />
             </svg>
           </div>
-          <div class="profile-icon" onClick={() => navigate("/profile")}>
+          <div className="profile-icon" onClick={() => setOpen(prev => !prev)}>
             <svg viewBox="0 0 16 16">
               <path d="M8 7C9.65685 7 11 5.65685 11 4C11 2.34315 9.65685 1 8 1C6.34315 1 5 2.34315 5 4C5 5.65685 6.34315 7 8 7Z" />
               <path d="M14 12C14 10.3431 12.6569 9 11 9H5C3.34315 9 2 10.3431 2 12V15H14V12Z" />
             </svg>
           </div>
+            <Profile  open={open}
+                         onClose={() =>setOpen(false)}
+                         />
         </div>
       </div>
 
